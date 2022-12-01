1774d732fb31d12e459045122ca801d2eb74f037
Dmitrj Zhuravlev <zyzy84@rambler.ru>
2022-12-01 19:06:28 +0300

    Task2_finish

--- HomeWork_6/Task_2/Program.cs
+++ HomeWork_6/Task_2/Program.cs
@@ -2,20 +2,20 @@
 заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
 
-int b1 = ReadInt("Введите первое число: ");
-int k1 = ReadInt("Введите второе число: ");
-int b2 = ReadInt("Введите третье число: ");
-int k2 = ReadInt("Введите четвертое число: ");
+int b1 = ReadInt("Введите B1: ");
+int k1 = ReadInt("Введите K1: ");
+int b2 = ReadInt("Введите B2: ");
+int k2 = ReadInt("Введите K2: ");
 
 if (k1 == k2)
 {
     Console.Write("Прямые параллельны");
     return;
 }
-double count1 = b1 - b2;
-double count2 = k2 - k1;
+double temp1 = b1 - b2;
+double temp2 = k2 - k1;
 
-double x = count1 / count2;
+double x = temp1 / temp2;
 double y = k2 * x + b2;
 
 Console.Write($"({x}; {y})");
